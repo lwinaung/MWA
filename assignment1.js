5dd9ee14b0454492742b33802afb7474ee848056
lwinaung <40617236+lwinaung@users.noreply.github.com>
2018-06-28 21:23:12 -0500

    Modify assignment.js to add debounceTime.
    Modify assignment.js to add debounceTime.

--- assignment1.js
+++ assignment1.js
@@ -19,14 +19,49 @@ fetch(url).then(function (resp) {
     const { map, filter, debounceTime, throttleTime } = rxjs.operators;
     // console.log(data);
     const obs = fromEvent(input, 'keyup');
-    obs.subscribe((e) => console.log(e.target.value));
+
+    obs
+        .pipe(
+            debounceTime(400)
+        )
+        .subscribe((e) => {
+
+            if (e.target.value != "") {
+                console.log("Here");
+                from(data.teams) //If I want to show all objects, I have to use 'of' instead of 'from' because 'of' work on objects and 'from' works on values.
+                    .pipe(
+                        map(obj =>
+                            obj),
+                        filter(obj => obj.name.toUpperCase().startsWith(e.target.value.toUpperCase()))
+                        // filter(obj => obj.id === 27)
+                    )
+                    .subscribe(team => {
+                        console.log("Found One!");                        
+                        table.innerHTML = "";
+                        var tr = createNode('tr'),
+                            td = createNode('td'),
+                            div = createNode('div'),
+                            img = createNode('img'),
+                            span = createNode('span');
+                        img.src = team.flag;
+                        img.width = 40;
+                        span.innerHTML = team.name;
+
+                        append(div, img);
+                        append(div, span);
+                        append(td, div);
+                        append(tr, td);
+                        append(table, tr);
+                    });
+            }
+        });
+
     from(data.teams) //If I want to show all objects, I have to use 'of' instead of 'from' because 'of' work on objects and 'from' works on values.
         .pipe(
-            // debounceTime(1),
             map(obj =>
-                obj),
-        // filter(obj => obj.id === 27)
-    )
+                obj)
+            // filter(obj => obj.id === 27)
+        )
         .subscribe(team => {
             var tr = createNode('tr'),
                 td = createNode('td'),
@@ -47,4 +82,3 @@ fetch(url).then(function (resp) {
 })['catch'](function (error) {
     console.log(JSON.stringify(error));
 });
-
