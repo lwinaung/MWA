6742abc4caf3652bb6b751b3b58e0f7165fd04b7
lwinaung <40617236+lwinaung@users.noreply.github.com>
2018-07-14 10:53:40 -0500

    First commit for homework14.
    First commit for homework14.

---
+++ homework14/client/app/_services/user.service.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { appConfig } from '../app.config';
+import { User } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getById(_id: string) {
+        return this.http.get(appConfig.apiUrl + '/users/' + _id);
+    }
+
+    create(user: User) {
+        return this.http.post(appConfig.apiUrl + '/users/register', user);
+    }
+}
